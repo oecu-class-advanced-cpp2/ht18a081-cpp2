f713ff86cf8602d53e96cb0d15bcb9550cc16a77
y <y>
2020-12-05 19:15:23 +0900

    is_primeとmainを編集

--- ex-1-main.cpp
+++ ex-1-main.cpp
@@ -16,7 +16,7 @@ using namespace std;
 
 bool is_prime(int num) {
 	int count = 0;
-	if (num == 1) {
+	if (num == 1 || num < 0) {
 		return false;
 	}
 	for (int a = 2;a <= num - 1;a++) {
@@ -43,17 +43,17 @@ int nth_prime(unsigned int a, unsigned int d, unsigned int n) {
 
 int main() {
 	// 以下、同様に、入出力例通りになるか確認せよ。
-	std::cout<<nth_prime(367, 186, 151)<<std::endl;
-	std::cout << nth_prime(179, 10, 203) << std::endl;
-	std::cout << nth_prime(271, 37, 39) << std::endl;
-	std::cout << nth_prime(103, 230, 1) << std::endl;
-	std::cout << nth_prime(27, 104, 185) << std::endl;
-	std::cout << nth_prime(253, 50, 85) << std::endl;
-	std::cout << nth_prime(1, 1, 1) << std::endl;
-	std::cout << nth_prime(9075, 337, 210) << std::endl;
-	std::cout << nth_prime(307, 24, 79) << std::endl;
-	std::cout << nth_prime(331, 221, 177) << std::endl;
-	std::cout << nth_prime(259, 170, 40) << std::endl;
-	std::cout << nth_prime(269, 58, 102) << std::endl;
+	std::cout<<"入力: a:367,d:186,n:151 出力: "<<nth_prime(367, 186, 151)<<std::endl;
+	std::cout <<"入力: a:179,d:10,n:203 出力: "<< nth_prime(179, 10, 203) << std::endl;
+	std::cout <<"入力: a:271,d:37,n:39 出力: "<<nth_prime(271, 37, 39) << std::endl;
+	std::cout <<"入力: a:103,d:230,n:1 出力: "<<nth_prime(103, 230, 1) << std::endl;
+	std::cout <<"入力: a:27,d:104,n:185 出力: " << nth_prime(27, 104, 185) << std::endl;
+	std::cout <<"入力: a:253,d:50,n:85 出力: " << nth_prime(253, 50, 85) << std::endl;
+	std::cout <<"入力: a:1,d:1,n:1 出力: " << nth_prime(1, 1, 1) << std::endl;
+	std::cout <<"入力: a:9075,d:337,n:210 出力: " << nth_prime(9075, 337, 210) << std::endl;
+	std::cout <<"入力: a:307,d:24,n:79 出力: "<< nth_prime(307, 24, 79) << std::endl;
+	std::cout <<"入力: a:331,d:221,n:177 出力: "<< nth_prime(331, 221, 177) << std::endl;
+	std::cout <<"入力: a:259,d:170,n:40 出力: "<< nth_prime(259, 170, 40) << std::endl;
+	std::cout <<"入力: a:269,d:58,n:102 出力: " << nth_prime(269, 58, 102) << std::endl;
 	return 0;
 }
